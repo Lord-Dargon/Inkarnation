0fa513be4aa90ceaf6b2102f46d24a0063912623
Lord-Dargon <67993510+Lord-Dargon@users.noreply.github.com>
2026-02-06 20:12:38 -0500

    Label

--- Inkarnation/Scripts/tutorial.gd
+++ Inkarnation/Scripts/tutorial.gd
@@ -1,6 +1,19 @@
 extends Label
+@onready var timer: Timer = $Timer
 
 
 func _process(delta: float) -> void:
 	if visible:
 		visible_ratio += 0.01
+		
+		
+	if visible_ratio >= 0.75:
+		timer.start()
+	
+	
+
+
+func _on_timer_timeout() -> void:
+	hide()
+	visible_ratio = 0
+	pass # Replace with function body.
